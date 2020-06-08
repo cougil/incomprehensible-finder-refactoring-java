3999dc31232f03a523d471337a78ea3f4eefdb43
Nacho Cougil <nacho.cougil@dynatrace.com>
2020-06-08 20:12:13 +0200

    Better initialization in the setup

--- src/test/java/test/FinderTests.java
+++ src/test/java/test/FinderTests.java
@@ -1,59 +1,57 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
-
 import algorithm.Couple;
 import algorithm.Criteria;
 import algorithm.Finder;
 import algorithm.Person;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class FinderTests {
 
     private Person sue = new Person("Sue", new Date(50, 0, 1));
-    private Person greg = new Person( "Greg", new Date(52, 5, 1));
+    private Person greg = new Person("Greg", new Date(52, 5, 1));
     private Person sarah = new Person("Sarah", new Date(82, 0, 1));
     private Person mike = new Person("Mike", new Date(79, 0, 1));
-    private List<Person> list = new ArrayList<Person>();
+    private List<Person> list;
+    private Finder finder;
 
     @Before
-    public void setup() {
+    public void setUp() throws Exception {
+        list = new ArrayList<Person>();
+        finder = new Finder(list);
     }
 
     @Test
     public void Returns_Empty_Results_When_Given_Empty_List() {
-        Finder finder = new Finder(list);
 
         Couple couple = finder.find(Criteria.CLOSEST);
-        assertEquals(null, couple.youngest);
+        assertNull(couple.youngest);
 
-        assertEquals(null, couple.oldest);
+        assertNull(couple.oldest);
     }
 
     @Test
     public void Returns_Empty_Results_When_Given_One_Person() {
         list.add(sue);
 
-        Finder finder = new Finder(list);
-
         Couple couple = finder.find(Criteria.CLOSEST);
 
-        assertEquals(null, couple.youngest);
-        assertEquals(null, couple.oldest);
+        assertNull(couple.youngest);
+        assertNull(couple.oldest);
     }
 
     @Test
     public void Returns_Closest_Two_For_Two_People() {
         list.add(sue);
         list.add(greg);
-        Finder finder = new Finder(list);
 
         Couple couple = finder.find(Criteria.CLOSEST);
 
@@ -65,8 +63,6 @@ public class FinderTests {
     public void Returns_Furthest_Two_For_Two_People() {
         list.add(mike);
         list.add(greg);
-
-        Finder finder = new Finder(list);
 
         Couple couple = finder.find(Criteria.FARTHEST);
 
@@ -80,7 +76,6 @@ public class FinderTests {
         list.add(sarah);
         list.add(mike);
         list.add(greg);
-        Finder finder = new Finder(list);
 
         Couple couple = finder.find(Criteria.FARTHEST);
 
@@ -94,8 +89,6 @@ public class FinderTests {
         list.add(sarah);
         list.add(mike);
         list.add(greg);
-
-        Finder finder = new Finder(list);
 
         Couple couple = finder.find(Criteria.CLOSEST);
 
